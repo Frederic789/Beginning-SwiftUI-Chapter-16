bff285b7b16739ded20eb11507330a28fa74b5bf
Frederic789 <113332830+Frederic789@users.noreply.github.com>
2023-08-19 06:00:38 -0700

    Update Exercises.swift

--- Chapter16/Exercises.swift
+++ Chapter16/Exercises.swift
@@ -1,15 +1,62 @@
+//Modified Exercise.swift
 import SwiftUI
+import Combine
 
+class CharacterStore: ObservableObject {
+    @Published var characters: [Character] = []
+
+    init() {
+        characters = [
+            Character(name: "Warrior", hp: 100, dmg: 20),
+            Character(name: "Wizard", hp: 80, dmg: 30),
+            Character(name: "Rogue", hp: 60, dmg: 40)
+        ]
+    }
+}
+
+class Character: ObservableObject, Identifiable {
+    var id = UUID()
+    var name: String
+    var hp: Int
+    var dmg: Int
+
+    init(name: String, hp: Int, dmg: Int) {
+        self.name = name
+        self.hp = hp
+        self.dmg = dmg
+    }
+}
 
 struct ExercisesView: View {
-    @State var messageOne = ""
+  @State var messageOne = ""
+    @ObservedObject var characterStore = CharacterStore()
+
+   var body: some View {
+        NavigationView {
+            ForEach(characterStore.characters.indices, id: \.self) { index in
+                NavigationLink(destination: CharacterDetailView(character: $characterStore.characters[index])) {
+                    Text(characterStore.characters[index].name)
     
-    var body: some View {
-        VStack {
-            Text("Exercises")
-                .font(.largeTitle)
-            Text("Your work goes here")
+                }
+            }
+            .navigationTitle("RPG Characters")
         }
+    }
+}
+
+struct CharacterDetailView: View {
+//    @ObservedObject var character: Character
+    @Binding var character: Character
+
+    var body: some View {
+        Form {
+            Section(header: Text("Character Info")) {
+                TextField("Name", text: $character.name)
+                Stepper("HP: \(character.hp)", value: $character.hp, in: 1...999)
+                Stepper("DMG: \(character.dmg)", value: $character.dmg, in: 1...999)
+            }
+        }
+        .navigationTitle(character.name)
     }
 }
 
@@ -18,3 +65,4 @@ struct ExercisesView_Previews: PreviewProvider {
         ExercisesView()
     }
 }
+
